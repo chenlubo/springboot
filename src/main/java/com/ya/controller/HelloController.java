1f3a67e6c5e4cc291db78fa4e2efef9d7fe90666
15858818801@163.com <4454037.v>
2019-03-03 22:27:19 +0800

    hello

--- src/main/java/com/ya/controller/HelloController.java
+++ src/main/java/com/ya/controller/HelloController.java
@@ -1,9 +1,20 @@
 package com.ya.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * @Auther: Joker12138
  * @Date: 2019/3/3 22:21
  * @Description:
  */
+@Controller
 public class HelloController {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(){
+        return "hello";
+    }
 }
